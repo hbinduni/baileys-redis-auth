38a8c2ce2ae6141b58c08bf9c5a42958d901f741
Heriyanto Binduni <hbinduni@gmail.com>
2023-05-29 23:47:10 +0700

    first init

---
+++ src/Example/logger-pino.ts
@@ -0,0 +1,38 @@
+import pino from 'pino';
+
+import Pack from '../../package.json';
+
+const pretty = {
+  level: 'info',
+  target: 'pino-pretty',
+  options: {
+    colorize: true,
+    translateTime: 'SYS:isoDateTime',
+    ignore: 'pid,hostname',
+  },
+};
+
+const transports = pino.transport({
+  targets: [pretty],
+});
+
+const options = {
+  level: process.env.PINO_LOG_LEVEL || 'info',
+  formatters: {
+    bindings: (bindings) => {
+      return {
+        pid: bindings.pid,
+        host: bindings.hostname,
+        app: Pack.name,
+        v: Pack.version,
+        node_version: process.version,
+      };
+    },
+    level: (label, number) => {
+      return {level: number, label: label.toUpperCase()};
+    },
+  },
+  timestamp: pino.stdTimeFunctions.isoTime,
+};
+
+export const logger = pino(options, transports);
---
+++ src/index.ts
@@ -0,0 +1,76 @@
+import Redis, {RedisOptions} from 'ioredis';
+import {
+  AuthenticationCreds,
+  AuthenticationState,
+  SignalDataTypeMap,
+  initAuthCreds,
+  BufferJSON,
+  proto,
+} from '@whiskeysockets/baileys';
+
+/**
+ * Stores the full authentication state in Redis.
+ * */
+export const useRedisAuthState = async (
+  redisOptions: RedisOptions,
+  prefix: string = 'DB1'
+): Promise<{state: AuthenticationState; saveCreds: () => Promise<void>}> => {
+  const redis = new Redis(redisOptions);
+
+  const writeData = (data: any, key: string) => {
+    return redis.set(`${prefix}:${key}`, JSON.stringify(data, BufferJSON.replacer));
+  };
+
+  const readData = async (key: string) => {
+    try {
+      const data = await redis.get(`${prefix}:${key}`);
+      return data ? JSON.parse(data, BufferJSON.reviver) : null;
+    } catch (error) {
+      return null;
+    }
+  };
+
+  const creds: AuthenticationCreds = (await readData('creds')) || initAuthCreds();
+
+  return {
+    state: {
+      creds,
+      keys: {
+        get: async (type, ids) => {
+          const data: {[_: string]: SignalDataTypeMap[typeof type]} = {};
+          await Promise.all(
+            ids.map(async (id) => {
+              let value = await readData(`${type}-${id}`);
+              if (type === `${prefix}:app-state-sync-key` && value) {
+                value = proto.Message.AppStateSyncKeyData.fromObject(value);
+              }
+
+              data[id] = value;
+            })
+          );
+
+          return data;
+        },
+        set: async (data) => {
+          const pipeline = redis.pipeline();
+          for (const category in data) {
+            for (const id in data[category]) {
+              const value = data[category][id];
+              const key = `${prefix}:${category}-${id}`;
+              if (value) {
+                pipeline.set(key, JSON.stringify(value, BufferJSON.replacer));
+              } else {
+                pipeline.del(key);
+              }
+            }
+          }
+
+          await pipeline.exec();
+        },
+      },
+    },
+    saveCreds: async () => {
+      await writeData(creds, 'creds');
+    },
+  };
+};
